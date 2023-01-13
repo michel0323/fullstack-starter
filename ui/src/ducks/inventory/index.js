6601bea46df7d101e4988c51c3f8be105a5cea7d
michel0323 <87438059+michel0323@users.noreply.github.com>
2023-01-12 23:54:39 -0500

    Modified Reducer for create & update methods

--- ui/src/ducks/inventory/index.js
+++ ui/src/ducks/inventory/index.js
@@ -1,30 +1,58 @@
-import { createAction, handleActions } from 'redux-actions';
+import axios from 'axios'
+import { createLogger } from 'redux-logger'
+import DesignLayout from './containers/DesignLayout'
+import HomeContainer from './containers/HomeContainer'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDom from 'react-dom'
+import reducers from './reducers'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { thunkMiddleware } from './middleware'
+import { applyMiddleware, createStore } from 'redux'
+import 'core-js/es/map'
+import 'core-js/es/set'
 
-const actions = {
-	INVENTORY_GET_ALL: 'inventory/get_all',
-};
+axios.defaults.withCredentials = true
 
-export let defaultState = {
-	all: [],
-};
+const configPath = '/config.json'
+const defaultConfig = {
+  restAPIUrl: 'http://localhost:8080',
+  publicPath: '/',
+}
 
-export const findInventory = createAction(actions.INVENTORY_GET_ALL, () => {
-	//Last modified by Michel T. On 01/03/23
-	(dispatch, getState, config) =>
-		axios
-			.get(`${config.restAPIUrl}/inventory`)
-			.then((suc) => dispatch(refreshInventory(suc.data)));
-});
+//Modified by Michel T. On 01/03/23
+//Last Updated by Michel T. on 01/10/23
 
-export default handleActions(
-	{
-		[actions.INVENTORY_GET_ALL]: (state, action) => {
-			return {
-				...state,
-				all: action.payload,
-			};
-		},
-	},
+axios.get(`${configPath}`).then(
+  (success) => {
+    renderApplication(Object.assign(defaultConfig, success.data))
+  },
+  (failure) => {
+    renderApplication(defaultConfig)
+  }
+)
 
-	defaultState
-);
+const renderApplication = (overrideConfig) => {
+  const config = {
+    ...overrideConfig
+  }
+
+  let middleware = [thunkMiddleware(config)]
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, createLogger()]
+  }
+
+  const store = createStore(reducers, { config: config }, applyMiddleware(...middleware))
+
+  ReactDom.render(
+    <Provider store={store}>
+      <Router>
+        <DesignLayout>
+          <HomeContainer publicPath={config.publicPath}/>
+        </DesignLayout>
+      </Router>
+    </Provider>,
+    document.getElementById('app')
+  )
+}
